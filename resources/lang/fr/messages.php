2368b0dd6af285224e0d7f0f7ff23a7707bd469b
city_hunter <contact@henoc-djabia.com>
2020-11-08 16:02:09 +0000

    Added French support

---
+++ resources/lang/fr/messages.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'day' => 'Jour',
+    'week' => 'Semaine',
+    'month' => 'Mois',
+    'year' => 'Année',
+
+    'interval_description' => [
+        'day' => 'Quotidien|Tous les :count Jours',
+        'week' => 'Hebdomadaire|Tous les :count Semaines',
+        'month' => 'Mensuel|Tous les :count Mois',
+        'year' => 'Annuel|Tous les :count Ans',
+    ],
+];
